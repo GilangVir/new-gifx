fab327d4fcf55556c6dceb78f4242b8d3240450d
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-22 11:00:21 +0700

    add: default array key

--- config/models/Logger.php
+++ config/models/Logger.php
@@ -4,14 +4,14 @@ use App\Models\Helper;
 
 class Logger {
     
-    public static function client_log(array $data) {
+    public static function client_log(array $data = ['mbrid' => 0, 'module' => '', 'data' => [], 'message' => '', 'device' => 'website']) {
         DBHelper::insert("tb_log", [
             'LOG_MBR' => $data['mbrid'] ?? 0,
             'LOG_MODULE' => $data['module'] ?? "-",
             'LOG_DATA' => json_encode($data['data']),
-            'LOG_MESSAGE' => $data['message'],
+            'LOG_MESSAGE' => $data['message'] ?? NULL,
             'LOG_IP' => Helper::get_ip_address(),
-            'LOG_DEVICENAME' => 'website',
+            'LOG_DEVICENAME' => $data['device'] ?? "website",
             'LOG_DATETIME' => date("Y-m-d H:i:s"),
         ]);
     }   
