20d7617446995d0c7ee258c3194906641c816f6e
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-22 15:19:50 +0700

    Update step-2.php

--- client-tridentpro.com/doc/verif/step-2.php
+++ client-tridentpro.com/doc/verif/step-2.php
@@ -1,4 +1,9 @@
-<?php 
+<?php
+
+use App\Models\FileUpload;
+use App\Models\User;
+
+$isVerified = User::isVerified($user['MBR_ID']);
 // if(isset($_GET['lewati'])) {
 //     $update = mysqli_query($db, "UPDATE tb_member SET MBR_VERIF = -1, MBR_STS = -1 WHERE MBR_ID = ".$user['MBR_ID']."");
 //     if(!$update || !mysqli_affected_rows($db)) {
@@ -43,7 +48,7 @@
     
                             <div class="col-md-4" mb-2>
                                 <label for="date_of_birth" class="form-label required">Date of birth</label>
-                                <input type="date" name="date_of_birth" id="date_of_birth" max="<?php echo date("Y-m-d") ?>" class="form-control" value="<?php echo $user['MBR_TGLLAHIR'] ?? ""; ?>" placeholder="Date of birth" required>
+                                <input type="date" name="date_of_birth" id="date_of_birth" class="form-control" value="<?= date("Y-m-d", strtotime($user['MBR_TGLLAHIR'])) ?? ""; ?>" placeholder="Date of birth" required>
                             </div>
     
                             <div class="col-md-6 mb-2">
@@ -56,17 +61,17 @@
     
                             <div class="col-md-6 mb-2">
                                 <label for="" class="form-label required">No. Identity</label>
-                                <input type="number" name="no_idt" class="form-control" value="<?php echo $user['MBR_NO_IDT'] ?? ""; ?>" placeholder="NIK" required>
+                                <input type="number" name="no_idt" class="form-control" value="<?= $isVerified['MBRFILE_NUMBER'] ?? ""; ?>" placeholder="NIK" required>
                             </div>
 
                             <div class="col-md-6 mb-2">
                                 <label for="ktp_photo" class="form-label required">KTP photo</label>
-                                <input type="file" class="dropify" name="ktp_photo" id="ktp_photo" <?= (!empty($user['MBR_FILE_KTP']))? 'data-default-file="'.($aws_folder . $user['MBR_FILE_KTP']).'"' : ""; ?> data-min-width="480" data-min-height="640" data-max-file-size="2M" ata-allowed-file-extensions="png jpg jpeg">
+                                <input type="file" class="dropify" name="ktp_photo" id="ktp_photo" <?= (!empty($isVerified['MBRFILE_PHOTO1']))? 'data-default-file="'.(FileUpload::awsFile($isVerified['MBRFILE_PHOTO1'])).'"' : ""; ?> data-min-width="480" data-min-height="640" data-max-file-size="2M" ata-allowed-file-extensions="png jpg jpeg">
                             </div>
 
                             <div class="col-md-6 mb-2">
                                 <label for="selfie_photo" class="form-label required">Selfie photo</label>
-                                <input type="file" class="dropify" name="selfie_photo" id="selfie_photo" <?= (!empty($user['MBR_FILE_SELFIE']))? 'data-default-file="'.($aws_folder . $user['MBR_FILE_SELFIE']).'"' : ""; ?> data-min-width="480" data-min-height="640" data-max-file-size="4M" ata-allowed-file-extensions="png jpg jpeg">
+                                <input type="file" class="dropify" name="selfie_photo" id="selfie_photo" <?= (!empty($isVerified['MBRFILE_PHOTO2']))? 'data-default-file="'.(FileUpload::awsFile($isVerified['MBRFILE_PHOTO2'])).'"' : ""; ?> data-min-width="480" data-min-height="640" data-max-file-size="4M" ata-allowed-file-extensions="png jpg jpeg">
                             </div>
             
                             <div class="col-md-12 w-100">
