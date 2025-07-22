4a521537629122d9c019a692c6c46bf66bff358f
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-22 13:47:41 +0700

    Update SystemInfo.php

--- config/core/SystemInfo.php
+++ config/core/SystemInfo.php
@@ -6,4 +6,25 @@ class SystemInfo {
     public static function isDevelopment(): bool {
         return (ini_get('display_errors') == "1")? true : false; 
     }
+
+    public static function appMode(): string|bool {
+        global $_ENV;
+        return $_ENV['APP_MODE'] ?? false;
+    }
+
+    public static function refreshSession() {
+        if(session_status() === PHP_SESSION_NONE) {
+            session_set_cookie_params([
+                'lifetime' => 3600, // 1Jam
+                'path' => "/",
+                'domain' => "",
+                'secure' => false,
+                'httponly' => true,
+                'samesite' => "Lax"
+            ]);
+
+            session_start();
+            session_regenerate_id();
+        }
+    }
 }
