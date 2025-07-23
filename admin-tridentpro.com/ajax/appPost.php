621cfa65fffe6a0f6735b1cd0183131366853a74
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-23 09:33:33 +0700

    Create appPost.php

---
+++ admin-tridentpro.com/ajax/appPost.php
@@ -0,0 +1,57 @@
+<?php
+require_once __DIR__ . "/../../config/setting.php";
+require_once CONFIG_ROOT . "/vendor/autoload.php";
+use App\Models\Admin;
+use Config\Core\Database;
+
+try {
+    $db = Database::getConnection();
+    $parseUrl = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $requestUri = str_replace(['\*', '/ajax', '/post'], ['', '', '/postdata'], $parseUrl);
+    $fileUrl = __DIR__ . $requestUri . ".php";
+
+    if(!file_exists($fileUrl)) {
+        JsonResponse([
+            'code'      => 404,
+            'success'   => false,
+            'message'   => "Page NotFound",
+            'data'      => []
+        ]);
+    }
+
+    if(empty($_SERVER['HTTP_REFERER'])) {
+        JsonResponse([
+            'code'      => 403,
+            'success'   => false,
+            'message'   => "Invalid Request",
+            'data'      => []
+        ]);
+    }
+
+    /** Authentication */
+    $user = Admin::authentication();
+    if(empty($user)) {
+        JsonResponse([
+            'code'      => 403,
+            'success'   => false,
+            'message'   => "Invalid User",
+            'data'      => []
+        ]);
+    }
+
+    $authorizedPermission = Admin::getAuthrorizedPermissions($user['ID_ADM']);
+    $url = str_replace("/postdata", "", $requestUri);
+    require_once $fileUrl;
+
+} catch (Exception $e) {
+    if(ini_get("display_errors") == "1") {
+        throw $e;
+    }
+
+    JsonResponse([
+        'code'      => 200,
+        'success'   => false,
+        'message'   => "Internal Server Error",
+        'data'      => []
+    ]);
+}
