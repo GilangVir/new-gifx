4f0a82060571df9c7f412cf86ef268e1724bcc72
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-23 13:28:51 +0700

    implementasi ajax action create dari shared permission-group

---
+++ config/shared/AdminPermission/views/permission-group/ajax/create.php
@@ -0,0 +1,61 @@
+<?php
+use App\Models\Admin;
+use App\Models\Helper;
+use App\Models\DBHelper;
+
+if(!$isAllowed) {
+    JsonResponse([
+        'code'      => 200,
+        'success'   => false,
+        'message'   => "Authorization Failed",
+        'data'      => []
+    ]);
+}
+
+$data = Helper::getSafeInput($_POST);
+foreach(['group_name', 'group_type', 'group_icon'] as $req) {
+    if(empty($data[ $req ])) {
+        JsonResponse([
+            'code'      => 200,
+            'success'   => false,
+            'message'   => "{$req} diperlukan",
+            'data'      => []
+        ]);
+    }
+}
+
+/** Get Max ID */
+$order = Admin::maxGroupId();
+if($order == 0) {
+    JsonResponse([
+        'code'      => 200,
+        'success'   => false,
+        'message'   => "Gagal membuat nomor order",
+        'data'      => []
+    ]);
+}
+
+/** Insert */
+$insert  = DBHelper::insert("admin_module_group", [
+    'group' => $data['group_name'],
+    'type' => $data['group_type'],
+    'icon' => $data['group_icon'],
+    'min_level' => 0,
+    'order' => $order
+]);
+
+if(!$insert) {
+    JsonResponse([
+        'code'      => 200,
+        'success'   => false,
+        'message'   => "Gagal membuat modul grup baru",
+        'data'      => []
+    ]);
+}
+
+JsonResponse([
+    'code'      => 200,
+    'success'   => true,
+    'message'   => "Berhasil membuat modul grup baru",
+    'data'      => []
+]);
