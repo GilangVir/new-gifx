9d760dbc2f240e6e505dd58b9fdde7d6847b71d4
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-23 09:33:53 +0700

    Create appDatatable.php

---
+++ admin-tridentpro.com/ajax/appDatatable.php
@@ -0,0 +1,76 @@
+<?php
+if(session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+require_once __DIR__ . "/../../config/setting.php";
+use Ozdemir\Datatables\Datatables;
+use Ozdemir\Datatables\DB\MySQL;
+use App\Models\Helper;
+use App\Models\Admin;
+use Config\Core\Database;
+
+try {
+    global $_SESSION, $_COOKIE;
+    global $db;
+    $aws_folder = "https://allmediaindo-2.s3.ap-southeast-1.amazonaws.com/gfsprime/";
+    $dt = new Datatables( new MySQL([ 
+        'host'     => Database::$host,
+        'port'     => Database::$port,
+        'username' => Database::$username,
+        'password' => Database::$password,
+        'database' => Database::$database
+    ]));
+    
+
+    /** validate token */
+    $user = Admin::authentication();
+    if(empty($user)) {
+        JsonResponse([
+            'code'      => 404,
+            'success'   => false,
+            'message'   => "Invalid User",
+            'data'      => []
+        ]);
+    }
+
+    /** Admin Permission */
+    $authorizedPermission = Admin::getAuthrorizedPermissions($user['ID_ADM']);
+    $requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $url = str_replace("/ajax/datatable", "", $requestUri);
+    $permission = Admin::hasPermission($authorizedPermission, $url);
+    if(empty($_SERVER['HTTP_REFERER'])) {
+        JsonResponse([
+            'code'      => 404,
+            'success'   => false,
+            'message'   => "Invalid Request",
+            'data'      => []
+        ]);
+    }
+
+
+    /** Validate filename */
+    if(!$permission) {
+        JsonResponse([
+            'code'      => 403,
+            'success'   => false,
+            'message'   => "Invalid Permission",
+            'data'      => []
+        ]);
+    }
+
+    $fileUrl = str_replace("/view", "", $permission['fileurl']);
+    if(!file_exists(__DIR__ . "/tabledata/{$fileUrl}.php")) {
+        JsonResponse([
+            'code'      => 404,
+            'success'   => false,
+            'message'   => "Invalid Url",
+            'data'      => []
+        ]);
+    }
+
+    require_once __DIR__ . "/tabledata/{$fileUrl}.php";
+
+} catch (Exception $e) {
+    throw $e;
+}
