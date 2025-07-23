40d1b4de31404a377511cda992b3a0a1382b4d73
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-24 02:55:24 +0700

    delete shared module

--- config/shared/InterfaceSharedView.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Shared;
-
-interface InterfaceSharedView {
-    
-    public static function render(string $filepath, array $data = []);
-    public static function render_script(string $filepath, array $data = []);
-
-}
