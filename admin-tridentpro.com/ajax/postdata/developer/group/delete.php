6c246556ee4df49f5e17cc6f7c85fff0af244b75
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-23 13:29:24 +0700

    implementasi ajax action delete dari shared permission-group

--- admin-tridentpro.com/ajax/postdata/developer/group/delete.php
+++ admin-tridentpro.com/ajax/postdata/developer/group/delete.php
@@ -1,54 +1,6 @@
 <?php
-use App\Models\Helper;
-use App\Models\Logger;
-use App\Models\Admin;
-use App\Models\DBHelper;
-use App\Models\PermissionGroup;
 use App\Shared\AdminPermission\Core\AdminPermissionCore;
-
-$permission = AdminPermissionCore::hasPermission($authorizedPermission, $url);
-if(!$permission) {
-    JsonResponse([
-        'code'      => 200,
-        'success'   => false,
-        'message'   => "Authorization Failed",
-        'data'      => []
-    ]);
-}
-
-$idGroup = Helper::form_input($_POST['id'] ?? "");
-$group = PermissionGroup::getById($idGroup);
-if(!$group) {
-    JsonResponse([
-        'code'      => 200,
-        'success'   => false,
-        'message'   => "Invalid ID",
-        'data'      => []
-    ]);
-}
-
-/** Update */
-$delete = DBHelper::delete("admin_module_group", ['id' => $group['id']]);
-if(!$delete) {
-    JsonResponse([
-        'code'      => 200,
-        'success'   => false,
-        'message'   => "Gagal menghapus grup",
-        'data'      => []
-    ]);
-}
-
-Logger::admin_log([
-    'admid' => $user['ADM_ID'],
-    'module' => "group",
-    'message' => "Menghapus module permission group ".$group['group'],
-    'ip' => Helper::get_ip_address(),
-    'data' => $group
-]);
-
-JsonResponse([
-    'code'      => 200,
-    'success'   => true,
-    'message'   => "Berhasil menghapus group",
-    'data'      => []
+App\Shared\AdminPermission\SharedViews::render_script("permission-group/ajax/delete", [
+    'isAllowed' => AdminPermissionCore::hasPermission($authorizedPermission, $url),
+    'user' => $user
 ]);
