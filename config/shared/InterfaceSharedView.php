8c0345af8b7f2798a62e8ca952ff7a65b8d8fb5c
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-23 13:27:54 +0700

    add: interface function render_script

--- config/shared/InterfaceSharedView.php
+++ config/shared/InterfaceSharedView.php
@@ -4,5 +4,6 @@ namespace App\Shared;
 interface InterfaceSharedView {
     
     public static function render(string $filepath, array $data = []);
+    public static function render_script(string $filepath, array $data = []);
 
 }
