de96ddc2a0807dac90c4fab1407cfd295041a381
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-23 15:51:06 +0700

    edit: render from shared permission-module/ajax/permission_update

---
+++ config/shared/AdminPermission/views/permission-module/ajax/permission_update.php
@@ -0,0 +1,98 @@
+<?php
+use App\Models\Helper;
+use App\Models\DBHelper;
+use App\Models\Logger;
+use App\Shared\AdminPermission\Core\AdminPermissionCore;
+use App\Shared\AdminPermission\Models\PermissionModule;
+
+$listGrup = AdminPermissionCore::availableGroup();
+if(!$isAllowed) {
+    JsonResponse([
+        'code'      => 200,
+        'success'   => false,
+        'message'   => "Authorization Failed",
+        'data'      => []
+    ]);
+}
+
+$data = Helper::getSafeInput($_POST);
+foreach(['code', 'module_id', 'permission_id', 'name', 'url'] as $req) {
+    if(empty($data[ $req ])) {
+        JsonResponse([
+            'success' => false,
+            'message' => "Kolom {$req} diperlukan",
+            'data' => []
+        ]);
+    }
+}
+
+/** check module */
+$module = PermissionModule::findModuleById($data['module_id']);
+if(!$module) {
+    JsonResponse([
+        'success' => false,
+        'message' => "ID Module tidak valid",
+        'data' => []
+    ]);
+}
+
+/** Check Permisson ID */
+if(is_numeric($data['permission_id']) === FALSE) {
+    JsonResponse([
+        'success' => false,
+        'message' => "Permission ID tidak valid",
+        'data' => []
+    ]);
+}
+
+$sqlGetPermission = $db->query("SELECT * FROM admin_permissions WHERE id = ".$data['permission_id']." LIMIT 1");
+$permission = $sqlGetPermission->fetch_assoc();
+if($sqlGetPermission->num_rows != 1) {
+    JsonResponse([
+        'success' => false,
+        'message' => "Permission ID tidak ditemukan",
+        'data' => []
+    ]);
+}
+
+/** check code */
+if($data['code'] != $permission['code']) {
+    if(AdminPermissionCore::isHavePermission($module['id'], $data['code']) !== FALSE) {
+        JsonResponse([
+            'success' => false,
+            'message' => "Kode permission sudah terdaftar",
+            'data' => []
+        ]);
+    }
+}
+
+/** Insert */
+$updateData = [
+    'code' => $data['code'],
+    'desc' => $data['name'],
+    'url' => $data['url']
+];
+
+$update = DBHelper::update("admin_permissions", $updateData, ['id' => $permission['id']]);
+if(!$update) {
+   JsonResponse([
+        'success' => false,
+        'message' => "Gagal memperbarui permission",
+        'data' => []
+    ]); 
+}
+
+Logger::admin_log([
+    'admid' => $user['ADM_ID'],
+    'module' => "module",
+    'message' => "Updated permission ".$data['code'].", module ".$module['module'],
+    'ip' => Helper::get_ip_address(),
+    'data' => $data
+]);
+
+
+JsonResponse([
+    'success' => true,
+    'message' => "Permission $data[code] berhasil diperbarui",
+    'data' => []
+]);
