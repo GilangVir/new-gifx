85f94a74267706248412a42e4e859f48c8a50509
Fani Annur Rozi <crazyhx4@gmail.com>
2025-07-28 17:09:47 +0700

    fix:

--- admin-tridentpro.com/ajax/postdata/developer/group/create.php
+++ admin-tridentpro.com/ajax/postdata/developer/group/create.php
@@ -1,4 +1,7 @@
 <?php
+
+use Allmedia\Shared\AdminPermission\Core\AdminPermissionCore;
+use App\Factory\PermissionGroupFactory;
 use App\Models\Helper;
 use App\Models\Logger;
 use Config\Core\Database;
@@ -12,37 +15,45 @@ if(!$adminPermissionCore->hasPermission($authorizedPermission, $url)) {
     ]);
 }
 
-/** Check Id */
-if(!$group) {
-    JsonResponse([
-        'code'      => 200,
-        'success'   => false,
-        'message'   => "{$req} diperlukan",
-        'data'      => []
-    ]);
-}
-
 $data = Helper::getSafeInput($_POST);
-foreach(['edit_group_id', 'edit_group_name', 'edit_group_type', 'edit_group_icon'] as $req) {
+$required = [
+    'group_name' => "Nama Grup",
+    'group_icon' => "Icon",
+    'group_type' => "Tipe Grup"
+];
+
+foreach($required as $req => $text) {
     if(empty($data[ $req ])) {
         JsonResponse([
             'code'      => 200,
             'success'   => false,
-            'message'   => "{$req} diperlukan",
+            'message'   => "{$text} diperlukan",
             'data'      => []
         ]);
     }
 }
 
-/** Update */
-$updateData = [
-    'group' => $data['edit_group_name'],
-    'type' => $data['edit_group_type'],
-    'icon' => $data['edit_group_icon']
-];
+/** check tipe */
+if(!in_array($data['group_type'], ['dropdown', 'single'])) {
+    JsonResponse([
+        'code'      => 200,
+        'success'   => false,
+        'message'   => "Tipe grup tidak valid",
+        'data'      => []
+    ]);
+}
 
-$update = Database::update("admin_module_group", $updateData, ['id' => $group['id']]);
-if(!$update) {
+/** Update */
+$maxOrder = PermissionGroupFactory::init()->maxGroupId() ?? 0;
+$insert = Database::insert("admin_module_group", [
+    'order' => $maxOrder,
+    'group' => $data['group_name'],
+    'type' => $data['group_type'],
+    'icon' => $data['group_icon'],
+    'min_level' => 0
+]);
+
+if(!$insert) {
     JsonResponse([
         'code'      => 200,
         'success'   => false,
@@ -54,14 +65,13 @@ if(!$update) {
 Logger::admin_log([
     'admid' => $user['ADM_ID'],
     'module' => "group",
-    'message' => "Memperbarui module group ".$data['edit_group_name'],
-    'ip' => Helper::get_ip_address(),
-    'data' => json_encode($data)
+    'message' => "Menambahkan group ".$data['group_name'],
+    'data' => $data
 ]);
 
 JsonResponse([
     'code'      => 200,
     'success'   => true,
-    'message'   => "Berhasil memperbarui data modul grup",
+    'message'   => "Berhasil membuat grup",
     'data'      => []
 ]);
