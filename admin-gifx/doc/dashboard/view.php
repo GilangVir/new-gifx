bc34f655f1acf5c7074abf5899e85be029d654e4
Fani Annur Rozi <crazyhx4@gmail.com>
2025-09-16 10:31:55 +0700

    Update view.php

--- admin-gifx/doc/dashboard/view.php
+++ admin-gifx/doc/dashboard/view.php
@@ -1,1 +1,9 @@
-ini dashboard
+<div class="page-header">
+    <div>
+        <h2 class="main-content-title tx-24 mg-b-5">Welcome To Dashboard</h2>
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?= pathbreadcrumb(0) ?>/dashboard">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+        </ol>
+    </div>
+</div>
