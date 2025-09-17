afe0e7a67fdb62d727fef498d79bfe5db1a7c34f
GilangViir <109290152+GilangVir@users.noreply.github.com>
2025-09-17 08:09:29 +0700

    tampilan view

--- admin-gifx/doc/master/negara/view.php
+++ admin-gifx/doc/master/negara/view.php
@@ -1,10 +1,44 @@
     
     <!-- tables -->
-
+<div class="row">
+    <div class="col-12 d-flex justify-content-center">
+        <div class="col mb-3 mt-5">
             <div class="card">
-                <h5 class="card-header" style="padding: 10px;">List Negara</h5>
+                <div class="card-header">
+                    <div class="card-body">
+                        <form>
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-control-label">COUNTRY NAME</label>
+                                <input type="" class="form-control" id="exampleFormControlInput1" placeholder="nama negara">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-control-label">CURRENCY</label>
+                                <input type="" class="form-control" placeholder="mata uang">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-control-label">CODE</label>
+                                <input type="" class="form-control" id="exampleFormControlInput1" placeholder="kode">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleFormControlInput1" class="form-control-label">PHONE CODE</label>
+                                <input type="" class="form-control" id="exampleFormControlInput1" placeholder="kode telepon">
+                            </div>
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col mb-3 mt-5">
+            <div class="card">
+                <div class="card-header d-flex justify-content-between">
+                    <h5>List Negara</h5>
+                    
+                </div>
                  <div class="card-body">
-                    <table class="table" id="countriesTable">
+                    <table class="table table-striped" id="countriesTable">
                         <thead>
                             <tr>
                             <th scope="col">COUNTRY NAME</th>
@@ -17,6 +51,9 @@
                     </table>
                 </div>
             </div>
+        </div>
+    </div>
+</div>
 
          
     <!-- tables -->
@@ -52,31 +89,3 @@
 </script>
 </body>
 </html>
-
-
- <!-- <script type="text/javascript">
-    let table;
-    $(document).ready(function() {
-        table = $('#table').DataTable({
-            processing: true,
-            serverSide: true,
-            deferRender: true,
-            scrollX: true,
-            ajax: {
-                url: "/ajax/datatable/admin/master/negara/view.php",
-                type: "GET"
-            },
-            columns: [
-                { data: 'COUNTRY_NAME' },
-                { data: 'COUNTRY_CURR' },
-                { data: 'COUNTRY_CODE' },
-                { data: 'COUNTRY_PHONE_CODE' },
-                { data: 'action', orderable: false, searchable: false } // Kolom action
-            ],
-            lengthMenu: [
-                [10, 50, 100, -1],
-                [10, 50, 100, "All"]
-            ]
-        });
-    });
-    </script> -->
